27e7c4bbc984d5ecccbe9b278e501ac5d0cad1e6
PProstacki <pprostacki@us.edu.pl>
2016-10-30 18:10:29 +0100

    Cwiczenie 1

--- src/textfiles/FileCounter.java
+++ src/textfiles/FileCounter.java
@@ -1,10 +1,31 @@
-
 package textfiles;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class FileCounter {
-    
-  
-    
-    
+
+    public int lineCount(File file) {
+        int lineCount = 0;
+        int previousChar = 0;
+        int currentChar = 0; // Powinno sie zaczynac od 1 zeby nie trza bylo robic entera przy ostatniej linii
+        try {
+            FileReader fr = new FileReader(file);
+
+            while ((currentChar = fr.read()) != -1) {
+                if (previousChar == 13 && currentChar == 10) {
+                    lineCount++;
+                }
+                previousChar = currentChar;
+            }
+
+            fr.close();
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+
+        return lineCount;
+    }
+
 }
--- src/textfiles/TextFiles.java
+++ src/textfiles/TextFiles.java
@@ -1,11 +1,12 @@
 package textfiles;
 
-
+import java.io.File;
 
 public class TextFiles {
 
     public static void main(String[] args) {
-
+        FileCounter fc = new FileCounter();
+        System.out.println(fc.lineCount(new File("nazwa.txt")));
     }
-    
+
 }

1303678963b5e468f0d6b178263cebe5581e0684
PProstacki <pprostacki@us.edu.pl>
2016-10-31 10:43:02 +0100

    2 Czesc cwiczenia 1

--- src/textfiles/FileCounter.java
+++ src/textfiles/FileCounter.java
@@ -1,5 +1,6 @@
 package textfiles;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -27,5 +28,29 @@ public class FileCounter {
 
         return lineCount;
     }
-
+    
+    public int wordCount(File file){
+        int wordCount = 0;
+        String line = "";
+        
+        try{
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            line = br.readLine();
+            while(line != null){
+                wordCount++;
+                for(int x = 0;x < line.length() - 1; x++){
+                    if(line.charAt(x) == ' ' || line.charAt(x) == '\t' || line.charAt(x) == '\n'){
+                        wordCount++;
+                    }
+                }
+                line = br.readLine();
+            }
+            
+            br.close();
+        }catch(IOException x){
+            x.printStackTrace();
+        }
+        
+        return wordCount;
+    }
 }
--- src/textfiles/TextFiles.java
+++ src/textfiles/TextFiles.java
@@ -6,7 +6,8 @@ public class TextFiles {
 
     public static void main(String[] args) {
         FileCounter fc = new FileCounter();
-        System.out.println(fc.lineCount(new File("nazwa.txt")));
+        System.out.println("Liczba wierszy: " + fc.lineCount(new File("nazwa.txt")));
+        System.out.println("Liczba słów: " + fc.wordCount(new File("nazwa.txt")));
     }
 
 }
